f62e0ccdd7c1a897cf9d09fc80a3ebaceef95dc7
SirNando <matiasnandin@hotmail.com>
2023-09-25 16:54:54 -0300

    Added key-based dynamic lists

--- first-react-app/src/components/Expenses/Expenses.jsx
+++ first-react-app/src/components/Expenses/Expenses.jsx
@@ -1,32 +1,40 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props) {
-return (
-    <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          date={props.expenses[0].date}
-          amount={props.expenses[0].amount}
+  const [filterYear, setFilterYear] = useState(2020);
+
+  // Computed value/state: derived from another state
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filterYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  }
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onExpenseFilter={filterChangeHandler}
+          selected={filterYear}
         />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          date={props.expenses[1].date}
-          amount={props.expenses[1].amount}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          date={props.expenses[2].date}
-          amount={props.expenses[2].amount}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          date={props.expenses[3].date}
-          amount={props.expenses[3].amount}
-        />
-    </Card>
-)
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;

8f25276d2e436759d4c976fc4b211db9b5a182f5
SirNando <matiasnandin@hotmail.com>
2023-12-15 12:44:01 -0300

    Finished react quiz third practice project

---
+++ third-practice-project/src/components/Question.jsx
@@ -0,0 +1,71 @@
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers";
+import { useState } from "react";
+import QUESTIONS from "../questions";
+
+const TIMER = 10000;
+
+export default function Question({
+  onSelectAnswer,
+  onSkipAnswer,
+  questionIndex,
+}) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  let timer = TIMER;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[questionIndex].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
+
+  return (
+    <div id="question">
+      <QuestionTimer
+        timeout={timer}
+        onExpire={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+        key={timer}
+      />
+      <h2>{QUESTIONS[questionIndex].text}</h2>
+      <Answers
+        answers={QUESTIONS[questionIndex].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
--- third-practice-project/src/components/Quiz.jsx
+++ third-practice-project/src/components/Quiz.jsx
@@ -1,56 +1,38 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
-import quizCompleteImage from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer";
-
-const TIMER = 10000;
+import Question from "./Question";
+import Summary from "./Summary";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
   const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback((selectedAnswer) => {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer];
     });
-  }, []);
+  },
+  []);
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
   }, [handleSelectAnswer]);
 
   if (quizComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImage} alt="The quiz is completed" />
-        <h2>Quiz complete!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
-  // Only attempt to access array if I'm not yet done with it
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={TIMER}
-          onExpire={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        questionIndex={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }

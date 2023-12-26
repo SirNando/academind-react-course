a08558b45211cfe1fdd586d9a351a04a447a215f
SirNando <matiasnandin@hotmail.com>
2023-12-26 12:14:59 -0300

    Added token handling logic for saving/deleting and showing UI elements conditionally

--- authentication/frontend/src/App.js
+++ authentication/frontend/src/App.js
@@ -13,13 +13,19 @@ import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
+import { tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: "token",
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -63,6 +69,11 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },

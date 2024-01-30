6d80b1610e94e0227e4cc8328ec349ea9938b1e7
SirNando <matiasnandin@hotmail.com>
2024-01-30 16:35:04 -0300

    Added an image picker, not found/error pages to nextjs project

---
+++ nextjs/app/components/meals/image-picker.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
+  const imagePicker = useRef();
+
+  function handleClick() {
+    imagePicker.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No picked image yet</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/peg"
+          name={name}
+          ref={imagePicker}
+          onChange={handleImageChange}
+          required
+        />
+      </div>
+      <button className={classes.button} onClick={handleClick} type="button">
+        Pick an Image
+      </button>
+    </div>
+  );
+}
